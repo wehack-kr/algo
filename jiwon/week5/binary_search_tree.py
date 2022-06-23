c451184722389daa1478b5577aef7eaad7040d65
Gxone <rhwldnjs1226@gmail.com>
2022-06-23 17:37:47 +0900

    Improve BST

--- jiwon/week5/binary_search_tree.py
+++ jiwon/week5/binary_search_tree.py
@@ -44,92 +44,91 @@ class BinarySearchTree:
         return
 
     def delete(self, value):
-        if self.root:
-            curr_node = self.root
-            delete_node = None
-            parent_node = None
-
-            # 탐색
-            while True:
-                if not curr_node:
-                    print('삭제할 노드가 없음')
-                    return
-
-                if curr_node.value == value:
-                    delete_node = curr_node
-                    break
-                else:
-                    if curr_node.value < value:
-                        parent_node = curr_node
-                        curr_node = curr_node.right
-                    else:
-                        parent_node = curr_node
-                        curr_node = curr_node.left
-                        
-            # 삭제
-            # delete_node에 하위 노드가 없는 경우
-            if not delete_node.left and not delete_node.right:
-                # delete_node가 최상위 노드일 경우
-                if not parent_node:
-                    self.root = None
-                    del delete_node
-                # 데이터가 부모 노드의 값보다 클 경우
-                elif parent_node.value > value:
-                    parent_node.left = None
-                    del delete_node
-                # 데이터가 부모 노드의 값보다 클 경우
-                else:
-                    parent_node.right = None
-                    del delete_node
-            # delete_node에 하위 노드가 존재하는 경우
-            else:
-                # 오른쪽 하위 노드가 존재할 경우
-                if not delete_node.left and delete_node.right:
-                    if not parent_node:
-                        self.root = delete_node.right
-                        del delete_node
-                    elif parent_node.value > value:
-                        parent_node.left = delete_node.right
-                        del delete_node
-                    else:
-                        parent_node.right = delete_node.right
-                        del delete_node
-                # 왼쪽 하위 노드가 존재할 경우
-                elif not delete_node.right and delete_node.left:
-                    if not parent_node:
-                        self.root = delete_node.left
-                        del delete_node
-                    elif parent_node.value > value:
-                        parent_node.left = delete_node.left
-                        del delete_node
-                    else:
-                        parent_node.right = delete_node.left
-                        del delete_node
-                # 둘 다 존재할 경우
-                else:
-                    change_node = delete_node.right
-
-                    # delete_node의 자리는 오른쪽 서브 트리의 가장 왼쪽에 있는 하위 노드로 대체함.
-                    # delete_node의 오른쪽 하위 노드의 왼쪽 하위 노드가 없을 경우 그대로 오른쪽 하위 노드가 change_node가 됨.
-                    if not change_node.left:
-                        # if not parent_node:
-                        delete_node.value = change_node.value
-                        delete_node.right = change_node.right
-                        del change_node
-                        # 2. root -> change_node, change_node.left = delete_node.left
-                    else:
-                        parent_change_node = None
-
-                        # 대체할 노드를 탐색
-                        while change_node.left:
-                            parent_change_node = change_node
-                            change_node = change_node.left
-                        
-                        delete_node.value = change_node.value
-                        parent_change_node.left = change_node.right
-                        del change_node
-        else:
+        if not self.root:
             print('삭제할 노드가 없음')
+            return
+
+        curr_node = self.root
+        delete_node = None
+        parent_node = None
+
+        # 탐색
+        while True:
+            if not curr_node:
+                print('삭제할 노드가 없음')
+                return
+
+            if curr_node.value == value:
+                delete_node = curr_node
+                break
+            elif curr_node.value < value:
+                parent_node = curr_node
+                curr_node = curr_node.right
+            else:
+                parent_node = curr_node
+                curr_node = curr_node.left
+
+        # 삭제
+        # delete_node에 하위 노드가 없는 경우
+        if not delete_node.left and not delete_node.right:
+            # delete_node가 최상위 노드일 경우
+            if not parent_node:
+                self.root = None
+            
+            # 데이터가 부모 노드의 값보다 클 경우
+            elif parent_node.value > value:
+                parent_node.left = None
+            
+            # 데이터가 부모 노드의 값보다 클 경우
+            else:
+                parent_node.right = None
+            
+            del delete_node
+        
+        # delete_node에 하위 노드가 존재하는 경우
+        else:
+            # 오른쪽 하위 노드가 존재할 경우
+            if not delete_node.left and delete_node.right:
+                if not parent_node:
+                    self.root = delete_node.right
+                elif parent_node.value > value:
+                    parent_node.left = delete_node.right
+                else:
+                    parent_node.right = delete_node.right
+                del delete_node
+            
+            # 왼쪽 하위 노드가 존재할 경우
+            elif not delete_node.right and delete_node.left:
+                if not parent_node:
+                    self.root = delete_node.left
+                elif parent_node.value > value:
+                    parent_node.left = delete_node.left
+                else:
+                    parent_node.right = delete_node.left
+                
+                del delete_node
+            
+            # 둘 다 존재할 경우
+            else:
+                change_node = delete_node.right
+
+                # delete_node의 자리는 오른쪽 서브 트리의 가장 왼쪽에 있는 하위 노드로 대체함.
+                # delete_node의 오른쪽 하위 노드의 왼쪽 하위 노드가 없을 경우 그대로 오른쪽 하위 노드가 change_node가 됨.
+                if not change_node.left:
+                    delete_node.value = change_node.value
+                    delete_node.right = change_node.right
+                else:
+                    parent_change_node = None
+
+                    # 대체할 노드를 탐색
+                    while change_node.left:
+                        parent_change_node = change_node
+                        change_node = change_node.left
+                    
+                    delete_node.value = change_node.value
+                    parent_change_node.left = change_node.right
+                
+                del change_node
 
 
 import random
@@ -146,8 +145,8 @@ bst.in_order_traverse(bst.root)
 print('')
 
 for n in range(len(nums)):
-    random_n = random.choice(nums)
-    bst.delete(random_n)
-    nums.remove(random_n)
+    random_num = random.choice(nums)
+    bst.delete(random_num)
+    nums.remove(random_num)
 
 bst.in_order_traverse(bst.root)
